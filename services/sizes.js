a0510ad26f6aa3f70baa46238177429fcab821ec
ulldecorb <ausias.bertran@gmail.com>
2022-04-26 23:12:23 +0200

    Install aframe dependency Create html file Create service folder

---
+++ services/sizes.js
@@ -0,0 +1,13 @@
+const planetRadius = [ 
+    { mercurio: 2439.7 }, 
+    { venus: 6051.8 },
+    { tierra: 6371 },
+    { mars: 3389.5 },
+    { jupiter: 69911 },    
+    { saturno: 58232 },
+    { urano: 25362 },
+    { neptuno: 24622 },
+];
+
+const sunRadius = { sun: 696340 };
+
